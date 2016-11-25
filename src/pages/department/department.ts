6c9cb01a758fe70e835d8eba095398ab6b2a7338
Wyatt Arent <hello@wyatt.ninja>
2016-11-24 23:01:23 -0700

    Update course viewing Populate departments with courses Divide courses between levels Style

--- src/pages/department/department.ts
+++ src/pages/department/department.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import $ from 'jqlite';
 import { NavController, ViewController, NavParams } from 'ionic-angular';
 
 @Component({
@@ -9,9 +9,23 @@ import { NavController, ViewController, NavParams } from 'ionic-angular';
 export class DepartmentPage {
 
   public dep: any;
+  public courses: any = {
+    'Graduate': [],
+    'Undergraduate': []
+  };
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public viewCtrl: ViewController) {
     this.dep = navParams.get('dep');
+    let courses = $(this.dep.raw.match(/\<table.*\/table>/)[0]).find('tr.odd,tr.even');
+    for (let i: number = 0; i < courses.length; i++) {
+      let a:any = $(courses[i]).find('a');
+      let level = a[2].innerText.trim();
+      let course = {
+        num: a[0].innerText.trim(),
+        name: a[1].innerText.trim()
+      };
+      this.courses[level].push(course);
+    }
   }
 
 }
