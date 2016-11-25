c485eacae4dff3f3e677b7b069bcb529a8f60fd7
Wyatt Arent <hello@wyatt.ninja>
2016-11-25 00:57:58 -0700

    Begin parsing course sidebar

--- src/pages/course-home/course-home.ts
+++ src/pages/course-home/course-home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import $ from 'jqlite';
 import { NavController, ViewController, NavParams } from 'ionic-angular';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'page-course-home',
@@ -9,9 +10,25 @@ import { NavController, ViewController, NavParams } from 'ionic-angular';
 export class CourseHomePage {
 
   public course: any;
+  public sidebar: any = [];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public viewCtrl: ViewController) {
+  constructor(public navCtrl: NavController, private http: Http, public navParams: NavParams, public viewCtrl: ViewController) {
     this.course = navParams.get('course');
+
+    http.get(`https://ocw.mit.edu${this.course.href}/`)
+      .subscribe(course => {
+        $(course.text().replace(/(\s\s)|\n|\t/g,'').match(/\<body.*\/body\>/)[0])
+          .find('nav#course_nav > ul > li')
+          .map(li => {
+            let first = $(li.firstChild);
+            if (first[0].localName == 'a') {
+              this.sidebar.push({
+                name: first[0].innerText.trim(),
+                href: first.attr('href')
+              });
+            }
+          });
+      });
   }
 
 }
